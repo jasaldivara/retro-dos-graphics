7d02b8849b63067e3a60b7d7b2700d61a4b50070
Jesús Abelardo Saldívar Aguilar <jasaldivara@gmail.com>
2020-01-28 16:15:41 -0600

    Script para crear imagen de disco con ejecutables MS-DOS

---
+++ creardisco.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+dd if=/dev/zero of=~/midisco.img bs=512 count=1440
+mkfs.msdos -F 12 -n "PROGRAMAS" -D 0x00 -v ~/midisco.img
+mcopy -i midisco.img ./bin/*.* ::
+mdir -i ~/midisco.img
+
+
