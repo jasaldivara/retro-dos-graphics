b87086cc5f253881f65c17d0050532a939347787
Jesús Abelardo Saldívar Aguilar <jasaldivara@gmail.com>
2020-01-30 18:58:33 -0600

    Speaker.asm: Usando notacion musical para partitura.

--- speaker.asm
+++ speaker.asm
@@ -5,6 +5,34 @@ CPU 8086
 
   org 100h
 
+  ; Notas musicales
+  ; Frecuencia, tercera octava
+  %define _C	131
+  %define _C#	139
+  %define _D	147
+  %define _D#	155
+  %define _E	165
+  %define _F	175
+  %define _F#	185
+  %define _G	196
+  %define _G#	208
+  %define _A	220
+  %define _A#	233
+  %define _B	245
+
+  ;
+  %define freqosc	1193180
+
+  ; macro mus(n,o)
+  ;
+  ; n = frecuencia de la nota en tercera octava
+  ; o = numero de octava que se desea
+  ;
+  ; Genera un numero de conteo para que el oscilador genere una frecuencia
+  ; segun la nota musical y la octava que se le proporcione
+
+  ;%define mus(n,o)	( freqosc / ( n << ( o−3 ) ) )
+  %define mus(n,o)	freqosc / (n << (o - 3))
 
   %macro SpeakerOn 0
 
@@ -176,33 +204,56 @@ section .data
   ;	duracion, nota
   partitura:	; Si la nota es cero: Guardar silencio
 
-  dw	4, 4554
-  dw	2, 4058
+  dw	4, mus( _C, 4 )
+  dw	2, mus( _D, 4 )
 
-  dw     1,7231
-  dw     1,6449
-  dw     2,6818
+  dw     1, mus( _E, 3 )
+  dw     1, mus( _F#, 3 )
+  dw     2, mus( _F, 3 )
 
-  dw	4, 3615
-  dw	2, 4058
+  dw	4, mus( _E, 4 )
+  dw	2, mus( _D, 4 )
 
-  dw     1,7231
-  dw     1,6449
-  dw     2,6818
+  dw     1, mus( _E, 3 )
+  dw     1, mus( _F#, 3 )
+  dw     2, mus( _F, 3 )
 
-  dw	4, 2875
-  dw	2, 3224
+  dw	4, mus( _G#, 4 )
+  dw	2, mus( _F#, 4 )
 
-  dw     1,7231
-  dw     1,6449
-  dw     2,6818
+  dw     1, mus( _E, 3 )
+  dw     1, mus( _F#, 3 )
+  dw     2, mus( _F, 3 )
 
-  dw	4, 2415
-  dw	2, 2875
+  dw	4, mus( _B, 4 )
+  dw	2, mus( _C, 5 )
 
-  dw     1,7231
-  dw     1,6449
-  dw     2,6818
+  dw     1, mus( _E, 3 )
+  dw     1, mus( _F#, 3 )
+  dw     2, mus( _F, 3 )
+
+  dw	0, 0		; Si la duración es cero: Fin de la música
+
+  nuevapart:
+  dw	4, mus( _C, 4 )
+  dw	4, mus( _D, 4 )
+  dw	2, mus( _F, 4 )
+  dw	2, mus( _E, 4 )
+
+  dw	4, mus( _C, 5 )
+  dw	4, mus( _D, 5 )
+  dw	2, mus( _F, 5 )
+  dw	2, mus( _E, 5 )
+
+  dw	4, mus( _C, 6 )
+  dw	4, mus( _D, 6 )
+  dw	2, mus( _F, 6 )
+  dw	2, mus( _E, 6 )
+
+  dw	4, mus( _C, 7 )
+  dw	4, mus( _D, 7 )
+  dw	2, mus( _F, 7 )
+  dw	2, mus( _E, 7 )
 
   dw	0, 0		; Si la duración es cero: Fin de la música
 
