0b6468850a49562c804dd96b91eb1b82343eb2ef
Jesús Abelardo Saldívar Aguilar <jasaldivara@gmail.com>
2020-03-11 18:25:47 -0600

    Generador de imagenes de disco, copiará también subdirectorios.

--- creardisco.sh
+++ creardisco.sh
@@ -2,7 +2,7 @@
 
 dd if=/dev/zero of=~/midisco.img bs=512 count=1440
 mkfs.msdos -F 12 -n "PROGRAMAS" -D 0x00 -v ~/midisco.img
-mcopy -i ~/midisco.img ./bin/*.* ::
+mcopy -s -i ~/midisco.img ./bin/** ::
 mdir -i ~/midisco.img
 
 
