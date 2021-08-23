44e7d69208d73e88036a6772d154fa6e5a380095
Jesús Abelardo Saldívar Aguilar <jasaldivara@gmail.com>
2021-08-22 19:29:15 -0500

    Arreglando problemas al crear imagen de disco

--- creardisco.sh
+++ creardisco.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
+touch ~/midisco.img
 dd if=/dev/zero of=~/midisco.img bs=512 count=1440
 mkfs.msdos -F 12 -n "PROGRAMAS" -D 0x00 -v ~/midisco.img
 mcopy -s -i ~/midisco.img ./bin/** ::
